6f6a6e4d5d1f0e2ca350829161d65330c1c6c989
ThanhLong <nguyenboo2018@gmail.com>
2023-10-20 15:46:24 +0700

    dadada

---
+++ DAL/Data/DentalContextDB1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Data
+{
+    public partial class DentalContextDB1 : Component
+    {
+        public DentalContextDB1()
+        {
+            InitializeComponent();
+        }
+
+        public DentalContextDB1(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
